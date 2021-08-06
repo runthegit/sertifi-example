59f16105cc1decac7c9c2997ac6fb737946214ba
runthegit <cwyman82@gmail.com>
2021-08-06 04:28:06 -0500

    more code cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,9 +14,6 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-  //let component: AppComponent;
-  //let fixture: ComponentFixture<AppComponent>;
-
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -27,24 +24,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h2').textContent).toContain('Sertifi Student Year Summary');
-  }));
-  /*
-  it("should use the quoteList from the service", () => {
-    const quoteService = fixture.debugElement.injector.get(QuoteService);
-    fixture.detectChanges();
-    expect(quoteService.getQuote()).toEqual(component.quoteList);
-  });
-  
-    it(`should have as title 'sertifi-example'`, () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      expect(app.title).toEqual('sertifi-example');
-    });
-  
-    it('should render title', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.nativeElement as HTMLElement;
-      expect(compiled.querySelector('.content span')?.textContent).toContain('sertifi-example app is running!');
-    }); */
+  })); 
 });
